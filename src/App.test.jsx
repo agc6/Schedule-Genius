ce60548cf9baacd779a9881bc2010c188dfe4b2c
Noah Masoud <114415606+nomasoud@users.noreply.github.com>
2024-03-14 15:51:58 -0500

    updated code

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders landing page elements', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Test for the presence of elements specific to your landing page
+  const titleElement = screen.getByText(/Schedule Genius/i);
+  expect(titleElement).toBeInTheDocument();
+
+  // You can add more assertions here as needed
 });
