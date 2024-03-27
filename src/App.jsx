c66f06ceb22cfc4914495583aa7b4b899a03cf2d
Noah Masoud <114415606+nomasoud@users.noreply.github.com>
2024-03-26 21:02:09 -0500

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Updated import statement
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
-      <div>
+    <div>
+      <BrowserRouter> {/* Fixed BrowserRouter */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </div>
-    </Router>
+      </BrowserRouter>
+    </div>
   );
 }
 
