eeaa7c710e717446c4a221e24dfab85091ddf9c1
Nil Ceylan <nfq3@txstate.edu>
2024-03-28 17:25:01 -0500

    Added js for branch to be visible

--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ToDoList = () => {
     return (
         <div>
-            <h2>Create a Unique To-Do List for you!</h2>
+            <h2><strong>Create a Unique To-Do List for you!</strong></h2>
             <p>This is the ToDoList page content.</p>
         </div>
     );
