8024b18d20f5dd2d9d7da01469df112bd97b673f
Nil Ceylan <nfq3@txstate.edu>
2024-03-31 23:32:05 -0500

    Updated ToDoList.jsx
    imported useState, set up toDoList function & added input field

--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState, useTransition} from 'react';
 import './ToDolist.css';
 const ToDoList = () => {
+    //ToDoList function
+    const ToDoList = () => {
+        const [tasks, setTasks] = useState([]); //Define tasks as array and function to update task
+        const [input, setInput] = useState(''); //Define user input and function to set input
+
+        //Function for user changing value of text field
+        const handleInputChange = (e) => {
+            setInput(e.target.value);
+        }
+    };
+
     return (
         <div className='todo-list'>
             <h2>To-Do List</h2>
             <div className='input-container'>
-                
+                <input 
+                type='Text' 
+                placeholder='Add a task...'
+                value={input}
+                onChange={handleInputChange}
+                />
+
             </div>
         </div>
     );
