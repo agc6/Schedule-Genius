2aa2a329bb212b36d88c2983424bb5ffbf1025a0
Noah Masoud <114415606+nomasoud@users.noreply.github.com>
2024-04-02 17:26:07 -0500

    todo list code added NF

--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -1,10 +1,34 @@
+import React, { useState } from 'react'
 import React from 'react';
 
 const ToDoList = () => {
+
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("")
+
+    function handleInputChange(event) {
+    }
+
+    function addTask() {
+    }
+
+    function deleteTask(index) {
+    }
+
+    function moveTaskDown(index) {
+    }
+
+    function moveTaskUp(index) {
+    }
+
+
     return (
-        <div>
-            <h2>Create a Unique To-Do List for you!</h2>
-            <p>This is the ToDoList page content.</p>
+        <div className='To-Do List'>
+            <h1>Create a Unique To-Do List for you!</h1>
+            <div>
+                <input
+                    type="text" />
+            </div>
         </div>
     );
 };
