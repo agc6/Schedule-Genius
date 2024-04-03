1c8c49f2af088d71f43e96741b5deb437d4e0750
Nil Ceylan <nfq3@txstate.edu>
2024-04-03 18:32:08 -0500

    imported default style sheet and set up func &  state vars

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,6 +1,13 @@
 import React, {useState} from "react";
 import Toggle from "react-toggle";
+import "react-toggle/style.css"; //Default toggle style sheet
 
 const DarkMode = () => {
+    const [darkMode, setDarkMode] = useState(false);    //DarkMode is false as default
 
-}
+    const toggleDarkMode = () =>{
+
+    };
+};
+
+export default DarkMode;
