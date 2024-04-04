4331d70516f85ff679424aa5a1475facecc25790
Noah Masoud <114415606+nomasoud@users.noreply.github.com>
2024-04-04 10:35:13 -0500

    Updated Home to take in header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link component
+import Header from '../components/Header'; // Import Header component
 import './Home.css'; // Correct path to Home.css
-import logo from '../images/logo.jpg'; // Adjusted path to logo.png
 import flatImage from '../images/flat.jpg'; // Adjusted path to flatImage.png
-//import { auth } from '../firebase/firebase-config'; /* TODO: Implement sign out functionality
-//import { signOut } from "firebase/auth"; /* TODO: Implement sign out functionality
-
-/* TODO: Implement sign out functionality
-const handleSignOut = async(event) => {
-    event.preventDefault(); // Prevent the default form submission
-    setError(''); // Clear any existing errors
-
-    try {
-        // Attempt to sign out
-        await signOut(auth);
-        // Redirect or do something upon successful sign out
-    } catch (error) {
-        console.error("error signing out", error);
-        setError(error.message); // TODO: implement error handling UI
-    }
-    console.log('Form submitted successfully!')
-};
-*/
 
 const Home = () => {
     return (
         <div>
-            <header>
-                <div className="container flex-row">
-                    <div className="header_logo">
-                        <img src={logo} alt="Schedule Genius Logo" />
-                    </div>
-                    <nav> {/* add signout button */}
-                        <ul className="header_menu flex-row">
-                            {/* Replace anchor tag with Link */}
-                            <li>
-                                <Link to="/about">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/features">Features</Link>
-                            </li>
-                            <li>
-                                <Link to="/support">Support</Link>
-                            </li>
-                            <li>
-                                <Link to="/todo">ToDoList</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <div className="right flex-center">
-                        {/* Replace anchor tag with Link */}
-                        <Link to="/signin" className="btn btn-secondary">Sign In</Link>
-                        <div className="menu-btn">
-                            <i className="fas fa-bars"></i>
-                        </div>
-                    </div>
-                </div>
-            </header>
+            <Header />
             <div className="fade_in">
                 <section id="sched" className="container flex-row">
                     <div className="sched__content">
