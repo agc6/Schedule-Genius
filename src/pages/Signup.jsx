712057df7f22835468c6c0d7fb74934afd6f937f
Noah Masoud <114415606+nomasoud@users.noreply.github.com>
2024-04-04 02:02:14 -0500

    updated these to have a redirect

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';  //useState to save user info that could change
+import React, { useState, useEffect } from 'react';  //useState to save user info that could change
 import './Signin.css'; // Reusing the CSS file from the Signin page for consistency
 import logo from '../images/logo.jpg'; // Using the same logo
-import { Link } from 'react-router-dom'; // Import Link component for SPA navigation
+import { Link, useNavigate } from 'react-router-dom'; // Import Link component for SPA navigation
 import validatePassword from '../components/passwordValidator'; //Import for password validator
 import { togglePasswordVisibility } from '../components/passwordVisibility'; //Import for password visibility
 import { auth } from '../firebase/firebase-config'; //Import the auth object from firebase-config
@@ -13,6 +13,7 @@ const SignupPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate(); // Access the navigate function to handle redirection
 
     //handlePasswordChange function that takes an event as an argument
     const handlePasswordChange = (event) => {
@@ -20,11 +21,11 @@ const SignupPage = () => {
         setErrorMessage(' ');   //Clears error message
     };
     //handleSubmit function for new users, takes an event as an argument
-    const handleSubmit = async(event) =>{
+    const handleSubmit = async (event) => {
         event.preventDefault(); //To prevent default form submission behavior
 
         const validateMessage = validatePassword(password); //Calls validatePassword to ensure it is correctly formatted
-        if(validateMessage !== 'Password is valid.'){
+        if (validateMessage !== 'Password is valid.') {
             setErrorMessage(validateMessage);   //If password not valid then set error message as validate message
             return;
         }
@@ -33,7 +34,7 @@ const SignupPage = () => {
         try {
             const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             console.log('userCredential:', userCredential.user); //If successful then log userCredential        
-        } catch(error){
+        } catch (error) {
             console.error("error signing up", error);   //If error occurs then log error
             setErrorMessage(error.message); //If error occurs then set error message as error message
         }
@@ -67,7 +68,7 @@ const SignupPage = () => {
                             <h1 className="heading">Create Your Account</h1>
                             <form onSubmit={handleSubmit} className="form">
                                 <div className="input-group">
-                                    <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                                    <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
                                     <label htmlFor="username">Username</label>
                                 </div>
                                 <div className="input-group">
@@ -77,16 +78,16 @@ const SignupPage = () => {
                                 <div className="input-group">
                                     <input type="password" id="password" value={password} onChange={handlePasswordChange} />
                                     <label htmlFor="password">Password</label>
-                                    <img src="https://svgshare.com/i/uqQ.svg" alt="" className="eye" onClick={togglePasswordVisibility}/>
+                                    <img src="https://svgshare.com/i/uqQ.svg" alt="" className="eye" onClick={togglePasswordVisibility} />
                                     <img
                                         src="https://svgshare.com/i/uqu.svg"
                                         alt=""
                                         className="close-eye"
-                                        style={{ display: 'none' }} 
+                                        style={{ display: 'none' }}
                                         onClick={togglePasswordVisibility}
                                     />
                                 </div>
-                                {errorMessage && <p className="error-messag">{errorMessage}</p>}
+                                {errorMessage && <p className="error-message">{errorMessage}</p>}
                                 <button className="btn">Sign Up</button>
                                 <p className="bottom-text">
                                     Already have an account?
