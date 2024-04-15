29372fd6e6a13ec69cd81f42ce8299c30dcb0a65
Evan Smith <ers131@txstate.edu>
2024-04-15 16:48:35 -0500

    learned how to update a collection

--- misc/resources/firebase-react-todo-app/src/App.js
+++ misc/resources/firebase-react-todo-app/src/App.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from './components/Todo';
+import { db } from './firebase';
+import { query, collection, onSnapshot } from 'firebase/firestore';
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -13,8 +15,23 @@ const style = {
 };
 
 function App() {
-  const [todos, setTodos] = useState(['Learn React', 'Grind Leetcode']); //eventually this will be from database
+  const [todos, setTodos] = useState([]); //eventually this will be from database
 
+//create todo
+//read todo from firebase
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArr = []
+      querySnapshot.forEach((doc) => {
+        todosArr.push({...doc.data(), id: doc.id})
+      });
+      setTodos(todosArr)
+    })
+    return () => unsubscribe
+  }, [])
+//update todo
+//delete todo
   return (
     <div className={style.bg}>
       <div className={style.container}>
