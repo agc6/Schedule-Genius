5992cb2f38e372940bf15a853202bf41874bf5de
Arely G <bbe11@txstate.edu>
2024-04-18 21:29:18 -0500

    added ability to click on dates and select them for task

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,7 +2,7 @@ import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback, useRef, useMemo } from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
@@ -42,50 +42,78 @@ const events = [
 function BigCalendar() {
     const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
     const [allEvents, setAllEvents] = useState(events);
+    const clickRef = useRef(null);
+
+    useEffect(() => {
+        return () => {
+            window.clearTimeout(clickRef.current);
+        };
+    }, []);
+
+    const onSelectSlot = useCallback((slotInfo) => {
+        window.clearTimeout(clickRef.current);
+        clickRef.current = window.setTimeout(() => {
+            setNewEvent({ ...newEvent, start: slotInfo.start, end: slotInfo.end });
+        }, 250);
+    }, [newEvent]);
 
     function handleAddEvent() {
-        
-        for (let i=0; i<allEvents.length; i++){
-
-            const d1 = new Date (allEvents[i].start);
+        for (let i = 0; i < allEvents.length; i++) {
+            const d1 = new Date(allEvents[i].start);
             const d2 = new Date(newEvent.start);
             const d3 = new Date(allEvents[i].end);
             const d4 = new Date(newEvent.end);
-      /*
-          console.log(d1 <= d2);
-          console.log(d2 <= d3);
-          console.log(d1 <= d4);
-          console.log(d4 <= d3);
-            */
 
-             if (
-              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
-                (d4 <= d3) )
-              )
-            {   
-                alert("CLASH"); 
-                break;
-             }
-    
+            if ((d1 <= d2 && d2 <= d3) || (d1 <= d4 && d4 <= d3)) {
+                alert("CLASH");
+                return;
+            }
         }
-        
-        
+
         setAllEvents([...allEvents, newEvent]);
     }
+
+    const defaultDate = useMemo(() => new Date(), []);
 
     return (
         <div className="calendar">
             <h1>Calendar</h1>
             <h2>Add New Event</h2>
             <div>
-                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
-                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
-                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                <input
+                    type="text"
+                    placeholder="Add Title"
+                    style={{ width: "20%", marginRight: "10px" }}
+                    value={newEvent.title}
+                    onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+                />
+                <DatePicker
+                    placeholderText="Start Date"
+                    style={{ marginRight: "10px" }}
+                    selected={newEvent.start}
+                    onChange={(start) => setNewEvent({ ...newEvent, start })}
+                />
+                <DatePicker
+                    placeholderText="End Date"
+                    selected={newEvent.end}
+                    onChange={(end) => setNewEvent({ ...newEvent, end })}
+                />
+                <button style={{ marginTop: "10px" }} onClick={handleAddEvent}>
                     Add Event
                 </button>
             </div>
-            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+            <div className="height600">
+                <Calendar
+                    defaultDate={defaultDate}
+                    events={allEvents}
+                    localizer={localizer}
+                    onSelectSlot={onSelectSlot}
+                    selectable
+                    startAccessor="start"
+                    endAccessor="end"
+                    style={{ height: 500, margin: "50px" }}
+                />
+            </div>
         </div>
     );
 }
