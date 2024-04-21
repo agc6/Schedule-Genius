7ad6af6c2cb0ca59080f60d07757f33b32477e82
Noah Masoud <114415606+nomasoud@users.noreply.github.com>
2024-04-20 21:10:41 -0500

    finishing touches to home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,19 +14,29 @@ const Home = () => {
                     <section id="sched" className="container flex-row">
                         <div className="sched__content">
                             <h1 className="title">Schedule Genius: Organize your college life efficiently</h1>
-                            <h2 className="text">Get your stuff done with <i><strong>ease. </strong></i>
-                                The ultimate tool for crafting the perfect schedule.</h2>
-                            <Link to="/signup" className="btn btn-secondary">Sign Up</Link>
-                        </div>
+                            <h2 className="text">
+                                Unlock your genius potential!<br />
+                                Get it done with <i><strong>ease.</strong></i>
+                            </h2>
+                            <Link to="/signup" className="btn btn-secondary">Sign Up</Link>                        </div>
                         <div className="sched__img">
                             <img src={flatImage} alt="Students using laptops." />
                         </div>
+                        <br />
+                        <br />
                     </section>
                 </div>
             </div>
+            <br />
+            <br />
+
 
             {/* Features Section */}
             <section id="features" className="features-section">
+                <br />
+                <br />
+                <br />
+
                 <div className="features-container">
                     <div className="feature">
                         <h3>Schedule Generator</h3>
@@ -48,31 +58,29 @@ const Home = () => {
                         <Link to="/signup">
                             <button className="btn-custom">Get to organizing!</button>
                         </Link>
+
                     </div>
                 </div>
             </section>
+            <br />
+
             {/* About Section */}
             <section id="about" className="about-section">
                 <div className='about-container'>
+                    <br />
                     <h2>About Page</h2>
                     <p>As a group of 4 enthusiastic students in Dr. Lehr's Software Engineering class, we successfully developed a scheduling app using React and Firebase.</p>
                     <p>Our aim is to empower students by providing a tool that helps them organize their assignments and work, fostering better time management skills..</p>
                     <br />
-                    <p>Please visit our features page for more info.</p>
-                    <br />
-                    <h3>Students that contributed:</h3>
-                    <p><i>Evan Smith</i></p>
-                    <p><i>Nil Ceylan</i></p>
-                    <p><i>Arely Gutierrez Carbajal</i></p>
-                    <p><i>Noah Masoud</i></p>
                 </div>
             </section>
 
             {/* Contact Section */}
             <section id="contact" className="contact-section">
+                <br />
                 <div className="contact-container">
                     <div className="contact-info">
-                        <h2>Get In Touch</h2>
+                        <h2>Contact Us</h2>
                         <p>
                             Please fill out the form below to send us an email and we will
                             get back to you as soon as possible.
@@ -81,19 +89,19 @@ const Home = () => {
                             <span>
                                 <i className="fa fa-map-marker"></i> Address
                             </span>
-                            {/* Display your address */}
+                            {"123 Team IO Street San Marcos, Texas 78666 "}
                         </div>
                         <div className="contact-item">
                             <span>
                                 <i className="fa fa-phone"></i> Phone
                             </span>{" "}
-                            {/* Display your phone number */}
+                            {"1+ (234)-567-8910"}
                         </div>
                         <div className="contact-item">
                             <span>
                                 <i className="fa fa-envelope-o"></i> Email
                             </span>{" "}
-                            {/* Display your email address */}
+                            {"support@schedulegenius"}
                         </div>
                     </div>
                     <div className="contact-form">
@@ -129,12 +137,19 @@ const Home = () => {
                             ></textarea>
                         </div>
                         <button type="submit" className="btn btn-custom btn-lg">
-                            Send Message
+                            Submit
                         </button>
                     </div>
                 </div>
             </section>
-        </div>
+
+            <footer class="footer">
+                <div class="container">
+                    <p>&copy; 2024 Schedule Genius. All rights reserved.</p>
+                    <p>Contributors: Evan Smith, Nil Ceylan, Arely Gutierrez Carbajal, Noah Masoud</p>
+                </div>
+            </footer >
+        </div >
     );
 };
 
